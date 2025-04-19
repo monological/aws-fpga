0c3b90ce3d8549e07385c6fec06bc51981be7962
Julian Lupu <monological@gmail.com>
2025-04-19 17:48:42 +0000

    Add a simple cl_hello_world example

---
+++ hdk/cl/examples/cl_hello_world/build/gen_afi.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+pwd=$(pwd)
+
+export DCP_BUCKET_NAME='abklabs-fpga-east'
+export DCP_FOLDER_NAME='dcp'
+export REGION='us-east-1'
+export DCP_TARBALL_TO_INGEST="$pwd/checkpoints/$2"
+export LOGS_BUCKET_NAME='abklabs-fpga-east'
+export LOGS_FOLDER_NAME='logs'
+export DCP_TARBALL_NAME=$(basename ${DCP_TARBALL_TO_INGEST})
+export CL_DESIGN_NAME="$1"
+export CL_DESIGN_DESCRIPTION="$1"
+
+echo $DCP_TARBALL_TO_INGEST
+
+aws s3 cp ${DCP_TARBALL_TO_INGEST} s3://${DCP_BUCKET_NAME}/${DCP_FOLDER_NAME}/
+
+aws ec2 create-fpga-image --name ${CL_DESIGN_NAME} --description "${CL_DESIGN_DESCRIPTION}" --input-storage-location Bucket=${DCP_BUCKET_NAME},Key=${DCP_FOLDER_NAME}/${DCP_TARBALL_NAME} --logs-storage-location Bucket=${LOGS_BUCKET_NAME},Key=${LOGS_FOLDER_NAME}/ --region ${REGION}
+
+

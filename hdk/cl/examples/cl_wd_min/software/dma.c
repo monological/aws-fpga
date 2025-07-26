0afb32c640433c20936525c5401f313fbda183f9
Julian Lupu <monological@gmail.com>
2025-07-25 18:10:54 -0700

    formatting, fix printf warnings

--- hdk/cl/examples/cl_wd_min/software/dma.c
+++ hdk/cl/examples/cl_wd_min/software/dma.c
@@ -5,61 +5,66 @@
 #include <stdarg.h>
 #include <sys/mman.h>
 #include <immintrin.h>
+#include <stdlib.h>
 
 uint64_t _wd_get_phys(void* p)
 {
     uint64_t PAGE_SIZE = (uint64_t)sysconf(_SC_PAGESIZE);
-    int pagemap_fd;
-    uint64_t vaddr;
-    uintptr_t vpn;
-    uint64_t pfn;
-
-    pagemap_fd = open("/proc/self/pagemap", O_RDONLY);
+    int pagemap_fd = open("/proc/self/pagemap", O_RDONLY);
     if (pagemap_fd < 0)
     {
-        printf (( "cannot open pagemap file: %d ", pagemap_fd ));
+        printf("cannot open pagemap file: %d\n", pagemap_fd);
         return 0;
     }
 
-    vaddr = (uint64_t)p;
-    vpn = vaddr / PAGE_SIZE;
-    for (size_t nread = 0; nread < sizeof(pfn); )
+    uint64_t vaddr = (uint64_t)p;
+    uint64_t vpn = vaddr / PAGE_SIZE;
+    uint64_t entry;
+
+    if (pread(pagemap_fd, &entry, sizeof(entry), vpn * sizeof(entry)) != sizeof(entry))
     {
-        ssize_t ret = pread(pagemap_fd, &pfn, sizeof(pfn) - nread, (off_t)((vpn * sizeof(pfn)) + nread));
-        if (ret <= 0)
-        {
-            printf (( "pread error: %lu ", ret ));
-            close(pagemap_fd);
-            return 0;
-        }
-        nread += (size_t)ret;
+        perror("pread error");
+        close(pagemap_fd);
+        return 0;
     }
-    pfn &= (1UL << 55) - 1;
-    pfn = (pfn * (long unsigned int)PAGE_SIZE) + (vaddr % (long unsigned int)PAGE_SIZE);
 
     close(pagemap_fd);
 
-    return pfn;
+    // Check if page is present
+    if (!(entry & (1ULL << 63)))
+    {
+        printf("Page not present in memory.\n");
+        return 0;
+    }
+
+    uint64_t pfn = entry & ((1ULL << 55) - 1);
+    return (pfn * PAGE_SIZE) + (vaddr % PAGE_SIZE);
 }
 
 int main()
 {
-    uint32_t* mem = mmap(0, 32, PROT_READ|PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_LOCKED, -1, 0);
-    uint64_t mem_p = _wd_get_phys(mem);
-    printf ("%p\n", mem_p);
+    const size_t MAP_SIZE = 32 * sizeof(uint32_t);
+    uint32_t* mem = mmap(NULL, MAP_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_LOCKED, -1, 0);
+    if (mem == MAP_FAILED) {
+        perror("mmap failed");
+        return 1;
+    }
 
-    for (int i = 0; i < 32; i ++)
+    uint64_t mem_p = _wd_get_phys(mem);
+    printf("Physical address: 0x%lx\n", mem_p);
+
+    for (int i = 0; i < 32; i++)
     {
         mem[i] = 0x0;
     }
 
     while (1)
     {
-        for (int i = 0; i < 32; i ++)
+        for (int i = 0; i < 32; i++)
         {
             uint32_t w = mem[i];
             if (w)
-                printf ("[%02d] = %08x\n", i, w);
+                printf("[%02d] = %08x\n", i, w);
         }
         sleep(1);
     }
